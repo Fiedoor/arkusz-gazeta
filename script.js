7faea4687e7ba3e85e74d78b72f9f0f507c0e4e6
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-10-17 13:40:16 +0200

    Skrypt działa

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function peekaboo(a) {
   n = document.querySelectorAll('.artykuł');
   n.forEach((element) => {
-    console.log(n);
     element.style.display = 'none';
   });
   document.getElementById(a).style.display = 'initial';
